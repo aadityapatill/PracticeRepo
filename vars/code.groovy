bd7ced3223b6d4b499075f68de7c47b4ad60e894
Aditya <adityapatil.rajesh@qapitol.com>
2023-07-11 08:02:42 +0530

    added jenkins shared libraries

--- vars/code.groovy
+++ vars/code.groovy
@@ -1,1 +1,22 @@
-def call(String branch, String cred, String env, String goal, String deploy)
+def cloneFromBranch(String branch, String goal, String cred, String email) {
+    if (branch == "main" || branch == "master") {
+      git branch: '$branch', url: 'https://github.com/aadityapatill/PracticeRepo'
+    } else {
+        println "skipping clone as branch '$branch' is not 'main' or 'master'"
+    }
+
+    switch (goal) {
+        case "build":
+           echo 'mvn build'
+            break
+        case "test":
+           echo 'mvn clean test'
+            break
+        case "deploy":
+            echo 'mvn deploy'
+            break
+        default:
+            echo 'no goals'
+            break
+    }
+}
