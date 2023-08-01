29e6dbbac57563e1c220654fcbbbdea3ef108e98
aadityapatill <133200645+aadityapatill@users.noreply.github.com>
2023-08-01 15:29:30 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,23 @@
-def call(Map pipelineParams){
+def call(Map params) {
+    def branch = params.branch
+    def url = params.url
+    def env = params.env
 
+    if (branch == null) {
+        error("Missing 'branch' parameter")
+        return
+    }
+
+    if (url == null) {
+        error("Missing 'url' parameter!")
+        return
+    }
+
+    if (env == null) {
+        error("Missing 'env' parameter!")
+        return
+    }
+  
+    echo "Testing branch: ${branch}, URL: ${url}, Language: ${env}"
 }
+
